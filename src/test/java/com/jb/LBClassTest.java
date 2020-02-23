fe0769e6b2fc701be1a90bde0133e0cd790c04a1
Jakub Boesche <jakub.boesche@gmail.com>
2020-02-23 21:58:27 +0100

    junit 4 and assertj

--- src/test/java/com/jb/LBClassTest.java
+++ src/test/java/com/jb/LBClassTest.java
@@ -1,27 +1,18 @@
 package com.jb;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.junit.Test;
 
 import static com.jb.LBClass.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-public class LBClassTest
-        extends TestCase {
-
-    public LBClassTest(String testName) {
-        super(testName);
-    }
-
-    public static Test suite() {
-        return new TestSuite(LBClassTest.class);
-    }
-
-    public void testShouldRecognizeClass() {
-        assertEquals(LESSTHAN100kB, LBClass.valueOf(1));
-        assertEquals(BETWEEN100kBAND1MB, LBClass.valueOf(132000));
-        assertEquals(BETWEEN1MAND10MB, LBClass.valueOf(5 * 1024 * 1024));
-        assertEquals(BETWEEN10MBAND100MB, LBClass.valueOf(50 * 1024 * 1024));
-        assertEquals(BETWEEN100MBAND1GB, LBClass.valueOf(500 * 1024 * 1024));
+public class LBClassTest {
+    @Test
+    public void shouldRecognizeClass() {
+        assertThat(LBClass.valueOf(1)).isEqualTo(LESSTHAN100kB);
+        assertThat(LBClass.valueOf(132000)).isEqualTo(BETWEEN100kBAND1MB);
+        assertThat(LBClass.valueOf(5 * 1024 * 1024)).isEqualTo(BETWEEN1MAND10MB);
+        assertThat(LBClass.valueOf(50 * 1024 * 1024)).isEqualTo(BETWEEN10MBAND100MB);
+        assertThat(LBClass.valueOf(500 * 1024 * 1024)).isEqualTo(BETWEEN100MBAND1GB);
     }
 }
--- src/test/java/com/jb/LastByteDistributionTest.java
+++ src/test/java/com/jb/LastByteDistributionTest.java
@@ -1,8 +1,6 @@
 package com.jb;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -15,112 +13,92 @@ import java.util.Map;
 
 import static com.jb.LBClass.BETWEEN100kBAND1MB;
 import static com.jb.LBClass.LESSTHAN100kB;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class LastByteDistributionTest
-        extends TestCase {
+public class LastByteDistributionTest {
     private static final String INPUT_FILE_NAME = "src/test/resources/testFile.csv";
     private static final String INPUT_FILE_NAME_INVALID = "src/test/resources/testFile_missing_column.csv";
-    private Path path;
+    private Path path = FileSystems.getDefault().getPath(INPUT_FILE_NAME);
 
-    public LastByteDistributionTest(String testName) {
-        super(testName);
-    }
-
-    public static Test suite() {
-        return new TestSuite(LastByteDistributionTest.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-
-        path = FileSystems.getDefault().getPath(INPUT_FILE_NAME);
-    }
-
-    public void testShouldConstructLastByteDistributionProcessor() {
-        //given
-        //when
+    @Test
+    public void shouldConstructLastByteDistributionProcessor() {
         LastByteDistributionProcessor processor = new LastByteDistributionFileProcessor(path);
-        //then
-        assertTrue(processor != null);
+
+        assertThat(processor).isNotNull();
     }
 
-    public void testShouldParseRow() throws IOException {
-        //given
+    @Test
+    public void shouldParseRow() throws IOException {
         LastByteDistributionFileProcessor processor = new LastByteDistributionFileProcessor(path);
-        //when
+
         List<LBEntry> outputDistribution = processor.parse();
-        //then
+
         //200,600000,2,2048,123.456,5
         LBEntry lbEntry = outputDistribution.get(0);
-        assertEquals(2 + 2048 + 5, lbEntry.getTtlb());
-        assertEquals(BETWEEN100kBAND1MB, lbEntry.getLbClass());
+        assertThat(lbEntry.getTtlb()).isEqualTo(2 + 2048 + 5);
+        assertThat(lbEntry.getLbClass()).isEqualTo(BETWEEN100kBAND1MB);
     }
 
-    public void testShouldThrowExceptionParsingInvalidRow() throws IOException {
-        //given
+    @Test
+    public void shouldThrowExceptionParsingInvalidRow() {
         Path invalidFilePath = FileSystems.getDefault().getPath(INPUT_FILE_NAME_INVALID);
         LastByteDistributionFileProcessor processor = new LastByteDistributionFileProcessor(invalidFilePath);
-        //when
-        try {
-            List<LBEntry> outputDistribution = processor.parse();
-            fail();
-        } catch (InvalidFileFormatException e) {
-            //then
-            // OK
-        }
+
+        assertThatThrownBy(() -> {
+            List<LBEntry> ignored = processor.parse();
+        }).isInstanceOf(InvalidFileFormatException.class);
     }
 
-    public void testShouldRunParser() throws IOException {
-        //given
+    @Test
+    public void shouldRunParser() throws IOException {
         LastByteDistributionFileProcessor processor = new LastByteDistributionFileProcessor(path);
-        //when
+
         List<LBEntry> entries = processor.parse();
-        //then
-        assertTrue(entries != null);
-        assertEquals(10, entries.size());
+
+        assertThat(entries).hasSize(10);
     }
 
-    public void testShouldCalculateStatistics() throws IOException {
-        //given
+    @Test
+    public void shouldCalculateStatistics() throws IOException {
         LastByteDistributionFileProcessor processor = new LastByteDistributionFileProcessor(path);
-        //when
+
         Map<LBClass, Map<Long, Double>> statistics = processor.calculateStatistics(processor.parse());
-        //then
-        assertTrue(statistics != null);
-        assertEquals(2, statistics.keySet().size());
+
+        assertThat(statistics).isNotNull();
+        assertThat(statistics).hasSize(2);
         double sum = statistics.values().stream()
                 .map(Map::values).flatMap(Collection::stream)
                 .mapToDouble(percentage -> percentage).sum();
-        assertEquals(100.0, sum);
-        assertEquals(10.0, statistics.get(LESSTHAN100kB).get(1L));
-        assertEquals(20.0, statistics.get(LESSTHAN100kB).get(6L));
-        assertEquals(10.0, statistics.get(LESSTHAN100kB).get(41L));
-        assertEquals(10.0, statistics.get(LESSTHAN100kB).get(61L));
-        assertEquals(10.0, statistics.get(BETWEEN100kBAND1MB).get(769L));
-        assertEquals(10.0, statistics.get(BETWEEN100kBAND1MB).get(2055L));
-        assertEquals(20.0, statistics.get(BETWEEN100kBAND1MB).get(7931L));
-        assertEquals(10.0, statistics.get(BETWEEN100kBAND1MB).get(30229L));
+        assertThat(sum).isEqualTo(100.0);
+        assertThat(statistics.get(LESSTHAN100kB).get(1L)).isEqualTo(10.0);
+        assertThat(statistics.get(LESSTHAN100kB).get(6L)).isEqualTo(20.0);
+        assertThat(statistics.get(LESSTHAN100kB).get(41L)).isEqualTo(10.0);
+        assertThat(statistics.get(LESSTHAN100kB).get(61L)).isEqualTo(10.0);
+        assertThat(statistics.get(BETWEEN100kBAND1MB).get(769L)).isEqualTo(10.0);
+        assertThat(statistics.get(BETWEEN100kBAND1MB).get(2055L)).isEqualTo(10.0);
+        assertThat(statistics.get(BETWEEN100kBAND1MB).get(7931L)).isEqualTo(20.0);
+        assertThat(statistics.get(BETWEEN100kBAND1MB).get(30229L)).isEqualTo(10.0);
     }
 
-    public void testShouldCreateXYChartFile() throws IOException {
-        //given
+    @Test
+    public void shouldCreateXYChartFile() throws IOException {
         LastByteDistributionFileProcessor processor = new LastByteDistributionFileProcessor(path);
-        //when
+
         Map<LBClass, Map<Long, Double>> statistics = processor.calculateStatistics(processor.parse());
         File chart = LBChartCreator.createChartFile(statistics, "testFile.png");
-        //then
-        assertTrue(chart != null);
+
+        assertThat(chart).isNotNull();
     }
 
-    public void testShouldCreateXYChartByteStream() throws IOException {
-        //given
+    @Test
+    public void shouldCreateXYChartByteStream() throws IOException {
         LastByteDistributionFileProcessor processor = new LastByteDistributionFileProcessor(path);
-        //when
+
         Map<LBClass, Map<Long, Double>> statistics = processor.calculateStatistics(processor.parse());
         ByteArrayOutputStream chartStream = LBChartCreator.createChartStream(statistics);
-        //then
-        assertTrue(chartStream != null);
-        assertTrue(chartStream.size() > 0);
+
+        assertThat(chartStream).isNotNull();
+        assertThat(chartStream.size()).isGreaterThan(0);
     }
 }
